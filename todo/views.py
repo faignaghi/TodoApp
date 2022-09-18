c99cf6921549e52a26128e9e6ee255b63deca43b
faignaghi <faignaghi@gmail.com>
2022-09-18 19:10:25 +0200

    todo view updated

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Welcome")
+    todos = Todo.objects.all()
+    context = {
+        "todos": todos
+    }
+    return render(request, "todo/home.html", context)
